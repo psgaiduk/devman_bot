d30698d7565ae54bcba6d64f8b2b3a9710d8f048
pavel <psgaiduk@gmail.com>
2022-01-28 22:02:43 +0300

    change start work bot from ifmain

--- main.py
+++ main.py
@@ -15,33 +15,38 @@ def get_reviews(url_, headers_, params_):
     return response.json()
 
 
-logger.info('Чат бот начал работу')
+def main():
+    logger.info('Чат бот начал работу')
 
-params = {}
-while True:
-    try:
-        reviews = get_reviews(URL, HEADERS, params)
-        if reviews.get('new_attempts'):
-            timestamp = reviews['last_attempt_timestamp']
-            new_attempts = reviews['new_attempts']
-            for new_attempt in new_attempts:
-                title = new_attempt['lesson_title']
-                result = new_attempt['is_negative']
-                link = new_attempt['lesson_url']
-                result_text = 'Преподавтель принял вашу работу'
-                if result:
-                    result_text = 'Ты не справился, нужно переделать'
-                BOT.send_message(
-                    text=f'Преподаватель проверил работу '
-                    f'"{title}"\n\n{result_text}\n{link}',
-                    chat_id=CHAT_ID)
-        else:
-            timestamp = reviews['timestamp_to_request']
-            params = {'timestamp': timestamp}
-    except exceptions.ReadTimeout as err:
-        logger.error(err, exc_info=True)
-    except exceptions.ConnectionError as err:
-        logger.error(err, exc_info=True)
-        time.sleep(1)
-    except Exception as err:
-        logger.error(err, exc_info=True)
+    params = {}
+    while True:
+        try:
+            reviews = get_reviews(URL, HEADERS, params)
+            if reviews.get('new_attempts'):
+                timestamp = reviews['last_attempt_timestamp']
+                new_attempts = reviews['new_attempts']
+                for new_attempt in new_attempts:
+                    title = new_attempt['lesson_title']
+                    result = new_attempt['is_negative']
+                    link = new_attempt['lesson_url']
+                    result_text = 'Преподавтель принял вашу работу'
+                    if result:
+                        result_text = 'Ты не справился, нужно переделать'
+                    BOT.send_message(
+                        text=f'Преподаватель проверил работу '
+                        f'"{title}"\n\n{result_text}\n{link}',
+                        chat_id=CHAT_ID)
+            else:
+                timestamp = reviews['timestamp_to_request']
+                params = {'timestamp': timestamp}
+        except exceptions.ReadTimeout as err:
+            logger.error(err, exc_info=True)
+        except exceptions.ConnectionError as err:
+            logger.error(err, exc_info=True)
+            time.sleep(1)
+        except Exception as err:
+            logger.error(err, exc_info=True)
+
+
+if __name__ == '__main__':
+    main()
